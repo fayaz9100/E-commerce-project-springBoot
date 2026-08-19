c66c67d037b449849bc107f4e14a9e68fe0b45cc
Fayaz Ahammed <shaikfayazahammed7@gmail.com>
2026-08-19 21:01:21 +0530

    Refactor JtSpringProjectApplicationTests to main class

--- src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -1,13 +1,20 @@
 package com.jtspringproject.JtSpringProject;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootTest
-class JtSpringProjectApplicationTests {
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+public class JtSpringProjectApplication extends SpringBootServletInitializer {
 
-	@Test
-	void contextLoads() {
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JtSpringProjectApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(JtSpringProjectApplication.class, args);
+    }
 }
