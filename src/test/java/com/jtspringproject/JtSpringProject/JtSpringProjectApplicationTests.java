a7198bebfb135de74d9cc8438d43fa2e9dd8dd40
Fayaz Ahammed <shaikfayazahammed7@gmail.com>
2026-08-19 21:10:51 +0530

    Refactor JtSpringProjectApplicationTests class
    Removed Spring Boot application configuration and main method.

--- src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -1,18 +1,5 @@
 package com.jtspringproject.JtSpringProject;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-public class JtSpringProjectApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return applicationpackage com.jtspringproject.JtSpringProject;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -21,11 +8,5 @@ class JtSpringProjectApplicationTests {
 
     @Test
     void contextLoads() {
-    }
-}.sources(JtSpringProjectApplication.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(JtSpringProjectApplication.class, args);
     }
 }
